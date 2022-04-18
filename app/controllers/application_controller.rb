3740dd3ca7896b8acc41ed72efa3662c9de753d5
Anjacodes <anja_schmidt7@posteo.de>
2022-04-18 12:04:13 -0500

    Add Authorization with CanCanCan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,10 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :email, :password, :current_password,
-               :password_confirmation, :bio, :photo)
-    end
+    # devise_parameter_sanitizer.permit(:account_update) do |u|
+    #   u.permit(:name, :email, :password, :current_password,
+    #            :password_confirmation, :bio, :photo)
+    # end
   end
+  
 end
