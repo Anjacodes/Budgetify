54921c125ad529c365e8ca05626103a772da9984
Anjacodes <anja_schmidt7@posteo.de>
2022-04-20 18:46:44 -0500

    User can only see categories they created

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,10 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_category, only: %i[show edit update destroy]
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.where(user_id: current_user.id)
     @category_expenses = []
 
     @categories.each do |category|
