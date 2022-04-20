f6aac764d605296ac21e3da6390110d955b820c0
Anjacodes <anja_schmidt7@posteo.de>
2022-04-20 14:38:45 -0500

    Devise notification fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password) }
     # devise_parameter_sanitizer.permit(:account_update) do |u|
     #   u.permit(:name, :email, :password, :current_password,
     #            :password_confirmation, :bio, :photo)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :expenses, dependent: :destroy
-  has_many :categories, dependent: :destroy
+  has_many :categories
 
   validates :name, presence: true
 end
